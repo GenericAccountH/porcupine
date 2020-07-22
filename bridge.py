d46e0e329cf5b59cd64fa01cffdad60c0dc510f2
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-22 08:30:16 -0500

    Create bridge.py

---
+++ bridge.py
@@ -0,0 +1,17 @@
+import time, logging
+from datetime import datetime
+import threading, collections, queue, os, os.path
+import deepspeech
+import numpy as np
+import pyaudio
+import wave
+import webrtcvad
+from halo import Halo
+from scipy import signal
+
+os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
+#read output, call voice detection from here
+#logic should be placed here 
+#porcupine should call this, which in turn should call deepspeech
+print("Callsign exit successful. Commence command script")
+os.system('python3 /home/pi/github/speech/mic_vad_streaming/mic_vad_streaming.py -m output_graph.tflite -l lm.binary -t trie -v 3')
