7a984ad16f5ade6811a2c2e5e7d9f764417e6188
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-22 14:59:36 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -11,14 +11,16 @@ from scipy import signal
 from subprocess import Popen, PIPE
 #TS_on = GPIO....
 callsign = False
+counter = 2
+i = 0
 
-
-#while(True):
-os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
-pipe = Popen("pwd", shell=True, stdout=PIPE).stdout
-output = pipe.read()
+while(i < counter):
+    os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
+    pipe = Popen("pwd", shell=True, stdout=PIPE).stdout
+    output = pipe.read()
+    i += 1
     #read output, call voice detection from here
 #logic should be placed here 
 #porcupine should call this, which in turn should call deepspeech
 #print("Callsign exit successful. Commence command script")
-os.system('python3 /home/pi/github/speech/mic_vad_streaming/mic_vad_streaming.py -m output_graph.tflite -l lm.binary -t trie -v 3')
+#os.system('python3 /home/pi/github/speech/mic_vad_streaming/mic_vad_streaming.py -m output_graph.tflite -l lm.binary -t trie -v 3')
