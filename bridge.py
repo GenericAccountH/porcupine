06587ab22d41580523c8e237450fc5bf607d814c
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-22 15:52:38 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -17,10 +17,11 @@ callsign = False
 counter = 2
 i = 0
 
-os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')>outputfile.py
+while(True):
+    os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')>outputfile.py
 
     #read output, call voice detection from here
 #logic should be placed here 
 #porcupine should call this, which in turn should call deepspeech
 #print("Callsign exit successful. Commence command script")
-os.system('python3 /home/pi/github/speech/mic_vad_streaming/mic_vad_streaming.py -m output_graph.tflite -l lm.binary -t trie -v 3')
+    os.system('python3 /home/pi/github/speech/mic_vad_streaming/mic_vad_streaming.py -m output_graph.tflite -l lm.binary -t trie -v 3')
