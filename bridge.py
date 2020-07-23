60b7c767dbd0a6945d73da7b0d9f9711b4e0b0d9
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-23 14:46:14 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -9,6 +9,7 @@ import webrtcvad
 from halo import Halo
 from scipy import signal
 
+import subprocess
 from subprocess import run
 import tempfile
 
@@ -18,10 +19,11 @@ counter = 2
 i = 0
 
 while(True):
-    animal = 7 + os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
+    animal = os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
     print(animal)
-    #read output, call voice detection from here
-#logic should be placed here 
-#porcupine should call this, which in turn should call deepspeech
-#print("Callsign exit successful. Commence command script")
+
+    proc=subprocess.Popen('echo "to stdout"', shell=True, stdout=subprocess.PIPE, )
+    output=proc.communicate()[0]
+    print output
+    
     os.system('python3 /home/pi/github/speech/mic_vad_streaming/mic_vad_streaming.py -m output_graph.tflite -l lm.binary -t trie -v 3')
