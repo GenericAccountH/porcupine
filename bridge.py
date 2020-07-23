e96fc866405d944263657a33cce2f8cdbadad01b
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-23 16:59:56 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -24,6 +24,7 @@ while(True):
     #animal = os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
     #print(animal)
     ou = os.popen('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn').read()
+    ou.close()
     print(ou)
 #     proc = subprocess.Popen(["python3", "/home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn"], stdout=subprocess.PIPE, shell=True)
 #     (out, err) = proc.communicate()
