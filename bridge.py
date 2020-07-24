8ef59c8505131ce36c661373356169a3313fd3eb
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-23 20:24:11 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -29,7 +29,7 @@ while(True):
     print(file1.read())
     
     print("Output of read by line function:")
-    print(file1.readline([0])
+    print(file1.readline([0]))
 #     ou = os.popen('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn').read()
 #     ou.close()
 #     print(ou)
