b34e4d42cb7e19157851fdcdd935c401b29afc23
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-23 20:33:22 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -29,9 +29,8 @@ while(True):
     print(file1.read())
     
     print("Output of read by line function:")
-    for i, line in enumerate(file1):
-        if(i == 1):
-            print(line)
+    lines = f.readlines()
+    print lines[2]
 #     ou = os.popen('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn').read()
 #     ou.close()
 #     print(ou)
