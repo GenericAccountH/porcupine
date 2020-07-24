4a9b9631081a946ac51a9be12d20d5e2c14f8d0c
GenericAccountH <67763290+GenericAccountH@users.noreply.github.com>
2020-07-24 18:04:06 -0500

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -11,25 +11,18 @@ from scipy import signal
 import sys
 import io
 
-import subprocess
-from subprocess import run
-import tempfile
+TS_on = False
+V_on = False
+command = False
+isClear = False
 
-
-callsign = False
-counter = 2
-i = 0
 targettext = "line 1"
 sourcetext = ""
 
 while(True):
-    animal = os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
-    print(animal)
+    os.system('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn')
     
     file1 = open('out.txt','r+')
-#     print("Output of read function:")
-#     print(file1.read())
-    
     print("Output of read by line function:")
     lines = file1.readlines()
     print(lines[0])
@@ -38,6 +31,8 @@ while(True):
         print(True)
     else:
         print(False)
+        
+    
     
 #     ou = os.popen('python3 /home/pi/github/porcupine/demo/python/porcupine_demo_mic.py --keyword_file_paths /home/pi/github/porcupine/resources/keyword_files/raspberry-pi/picovoice_raspberry-pi.ppn').read()
 #     ou.close()
